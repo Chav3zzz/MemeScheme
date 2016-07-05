36b962da05d8687a79e399e734c4df14d2599afe
Antonio Chavez <theantoniochavez@gmail.com>
2016-07-05 16:06:10 -0700

    Added start game with enter

--- game.js
+++ game.js
@@ -1,21 +1,36 @@
 // Image gets created inside of the canvas
 function playerImage() {
-    var gameCanvas = document.getElementById("memeCanvas");
+    var memeCanvas = document.getElementById("memeCanvas");
     var gameImage = new Image();
 
     gameImage.src = "pepeby45.png";
     memeCanvas.getContext("2d").drawImage(gameImage, Math.random() * 100, Math.random() * 100);
-
     memeCanvas.addEventListener("mousemove", drawPlayer);
 }
+
+// Starts the game by pressing the "enter key"
+window.addEventListener('keydown',function(eKey){
+  if(eKey.keyCode == 13 ){
+        playerImage();
+  }
+})
+
 
 
 // Image follows the mouse
 function drawPlayer(mouseEvent) {
-    var gameCanvas = document.getElementById("memeCanvas");
+    var memeCanvas = document.getElementById("memeCanvas");
     var gameImage = new Image();
 
     gameImage.src = "pepeby45.png";
     memeCanvas.width = 640; //clears the canvas
     memeCanvas.getContext("2d").drawImage(gameImage, mouseEvent.offsetX, mouseEvent.offsetY);
 }
+
+
+
+var canvas = document.getElementById("memeCanvas");
+  var context = canvas.getContext("2d");
+  context.fillStyle = "lime";
+  context.font = "bold 50px Arial";
+  context.fillText("press enter to start", 95, 250);
