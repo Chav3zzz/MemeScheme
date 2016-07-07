b68b54a6dee2d03802a2ec2b6d902ba82318ded8
Antonio Chavez <theantoniochavez@gmail.com>
2016-07-06 18:36:20 -0700

    The game is done, will be adding on features though.

--- game.js
+++ game.js
@@ -1,20 +1,21 @@
-var enemyYPositions = [0, -50, -75, -120, -250, -280, -305, -330, -340, -400, -600];
-var enemyXPositions = [250, 130, 300, 50, 190, 200, 220, 60, 100, 110, 500];
+var memeYPositions = [];
+var memeXPositions = [];
 
-var enemyY = 0;
+
 var playerPicX = 275;
 var playerPicY = 275;
 var gameImage = new Image();
 var memeImage = new Image();
 var enemyMeme = new Image();
+var timeLived = 0;
 
 
 // Image gets created inside of the canvas
 function playerImage() {
-    gameImage.src = "sadpepe50.png";
+    gameImage.src = "sadpepe40.png";
     memeCanvas.getContext("2d").drawImage(gameImage, Math.random() * 100, Math.random() * 100);
     memeCanvas.addEventListener("mousemove", drawPlayer);
-    setInterval(redrawMeme, 7);
+    setInterval(redrawMeme, 1);
 
 }
 
@@ -27,50 +28,55 @@ function drawPlayer(mouseEvent) {
 // Draws meme enemy onto the canvas
 function redrawMeme() {
     var currentEnemyNumber = 0;
-    var numberOfEnemies = enemyXPositions.length;
+    var numberOfEnemies = memeXPositions.length;
     enemyMeme.src = "bob100.png";
-    gameImage.src = "sadpepe50.png";
+    gameImage.src = "sadpepe40.png";
+
+
+//adds infinite memes to the array
+if (Math.random() < 3/70) {
+    memeYPositions.push(0);
+    memeXPositions.push(Math.random() * 800);
+}
 
   while (currentEnemyNumber < numberOfEnemies) {
-    enemyYPositions[currentEnemyNumber] = enemyYPositions[currentEnemyNumber] + 1;
+    memeYPositions[currentEnemyNumber] = memeYPositions[currentEnemyNumber] + 1;
     currentEnemyNumber = currentEnemyNumber + 1;
     }
 
-//   for (i = 0; i < numberOfEnemies; i++)
-//
-//
-//     Int i= 1;
-//   Int sum = 0;
-//
-//   while (sum < 10) {
-//   sum = sum + i;
-//   i++;
-//   }
-//
-// sum = 0;
-//
-// for(i = 0; i < 10; i++){
-// sum = sum + i;
-// document.write(sum);
-// }
-
-    memeCanvas.width = 640;     //this erases the contents of the canvas
+    memeCanvas.width = 800;     //this erases the contents of the canvas
     memeCanvas.getContext("2d").drawImage(gameImage, playerPicX, playerPicY);
 
     currentEnemyNumber = 0;
   while (currentEnemyNumber < numberOfEnemies) {
-    memeCanvas.getContext("2d").drawImage(enemyMeme, enemyXPositions[currentEnemyNumber], enemyYPositions[currentEnemyNumber]);
+    memeCanvas.getContext("2d").drawImage(enemyMeme, memeXPositions[currentEnemyNumber], memeYPositions[currentEnemyNumber]);
+    currentEnemyNumber = currentEnemyNumber + 1;
+  }
+
+    currentEnemyNumber = 0;
+  while (currentEnemyNumber < numberOfEnemies) {
+    if ( ( (playerPicX < memeXPositions[currentEnemyNumber] && memeXPositions[currentEnemyNumber] < playerPicX + 25) || (memeXPositions[currentEnemyNumber] < playerPicX && playerPicX < memeXPositions[currentEnemyNumber] + 40) ) && ( (playerPicY < memeYPositions[currentEnemyNumber] && memeYPositions[currentEnemyNumber] < playerPicY + 25) || (memeYPositions[currentEnemyNumber] < playerPicY && playerPicY < memeYPositions[currentEnemyNumber] + 32) ) ) {
+        alert("lol you loser. Distance: " + timeLived);
+        restart();
+    }
     currentEnemyNumber = currentEnemyNumber + 1;
     }
-}
+    timeLived = timeLived + 1;
+  }
 
+// Restarts the game when lost
+  function restart() {
+    memeXPositions = [];
+    memeYPositions = [];
+    timeLived = 0;
+}
 
 
 // Starts the game by pressing the "enter key"
 window.addEventListener('keydown',function(eKey){
   if(eKey.keyCode == 13 ){
     playerImage();
-    memeCanvas.width = 640; //clears the canvas
+    memeCanvas.width = 800; //clears the canvas
     memeCanvas.addEventListener("keydown", drawPlayer);
   }
 })
@@ -80,19 +86,4 @@ window.addEventListener('keydown',function(eKey){
   var context = canvas.getContext("2d");
     context.fillStyle = "lime";
     context.font = "bold 50px Arial";
-    context.fillText("press enter to start", 95, 250);
-
-
-
-    // // Draws meme enemy onto the canvas
-    // function redrawMeme() {
-    //     enemyY = enemyY + 1;    //increase enemyY variable by one pixel. If enemyY is 10, then enemyY + 1 is 11, etc.
-    //
-    //     enemyMeme.src = "bob100.png";
-    //     gameImage.src = "sadpepe50.png";
-    //
-    //     memeCanvas.width = 640;     //this erases the contents of the canvas
-    //     memeCanvas.getContext("2d").drawImage(gameImage, playerPicX, playerPicY);
-    //     memeCanvas.getContext("2d").drawImage(enemyMeme, 250, enemyY);
-    //     memeCanvas.getContext("2d").drawImage(enemyMeme, 100, enemyY);
-    // }
+    context.fillText("press enter to start", 175, 300);
